a65c107813904c193ebcb70a8c34f2377444d211
argipap <argipapaefstathiou@gmail.com>
2019-12-03 13:13:14 +0200

    localhost baseURL for cypress

--- test.sh
+++ test.sh
@@ -23,7 +23,7 @@ docker-compose down
 # run e2e tests
 docker-compose -f docker-compose-prod.yml up -d --build
 docker-compose -f docker-compose-prod.yml exec supplyit-users python manage.py recreate_db
-./node_modules/.bin/cypress run --config baseUrl=${localhost}
+./node_modules/.bin/cypress run --config baseUrl=http://localhost
 inspect $? e2e
 docker-compose -f docker-compose-prod.yml down
 
