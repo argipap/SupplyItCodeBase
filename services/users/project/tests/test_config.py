230e110630f63b019214cc2cfd81c0eebb3ddfb1
argipap <argipapaefstathiou@gmail.com>
2019-12-06 00:55:08 +0200

    swagger docs service

--- services/users/project/tests/test_config.py
+++ services/users/project/tests/test_config.py
@@ -33,7 +33,7 @@ class TestTestingConfig(TestCase):
         return app
 
     def test_app_is_testing(self):
-        self.assertTrue(app.config["SECRET_KEY"] == "my_precious")
+        self.assertTrue(app.config["SECRET_KEY"] == os.environ.get("SECRET_KEY"))
         self.assertTrue(app.config["TESTING"])
         self.assertFalse(app.config["PRESERVE_CONTEXT_ON_EXCEPTION"])
         self.assertTrue(
@@ -51,7 +51,7 @@ class TestProductionConfig(TestCase):
         return app
 
     def test_app_is_production(self):
-        self.assertTrue(app.config["SECRET_KEY"] == "my_precious")
+        self.assertTrue(app.config["SECRET_KEY"] == os.environ.get("SECRET_KEY"))
         self.assertFalse(app.config["TESTING"])
         self.assertFalse(app.config["DEBUG_TB_ENABLED"])
         self.assertTrue(app.config["TOKEN_EXPIRATION_DAYS"] == 30)
--- test.sh
+++ test.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 
+type=$1
+BASE_URL=$2
 fails=""
+
+if  [ -z "$BASE_URL" ]; then
+        BASE_URL="http://localhost"
+fi
 
 inspect() {
   if [ $1 -ne 0 ]; then
@@ -9,29 +15,74 @@ inspect() {
   fi
 }
 
-# run unit and integration tests
-docker-compose up -d --build
-docker-compose exec supplyit-users python manage.py test
-inspect $? supplyit-users
-docker-compose exec supplyit-users flake8 project
-inspect $? supplyit-users-lint
-docker-compose exec client npm run coverage
-inspect $? client
-docker-compose down
+# run server-side tests
+server() {
+  docker-compose up -d --build
+  docker-compose exec supplyit-users python manage.py test
+  inspect $? supplyit-users
+  docker-compose exec supplyit-users flake8 project
+  inspect $? supplyit-users-lint
+  docker-compose down
+}
 
-# new
+
+# run client-side tests
+client() {
+  docker-compose up -d --build
+  docker-compose exec client npm run coverage
+  inspect $? client
+  docker-compose down
+}
+
 # run e2e tests
-docker-compose -f docker-compose-prod.yml up -d --build
-docker-compose -f docker-compose-prod.yml exec supplyit-users python manage.py recreate_db
-./node_modules/.bin/cypress run --config baseUrl=http://localhost
-inspect $? e2e
-docker-compose -f docker-compose-prod.yml down
+e2e() {
+  docker-compose -f docker-compose-prod.yml up -d --build
+  docker-compose -f docker-compose-prod.yml exec supplyit-users python manage.py recreate_db
+#  ./node_modules/.bin/cypress run --config baseUrl=http://localhost
+  ./node_modules/.bin/cypress run --config baseUrl=${BASE_URL}
+  inspect $? e2e
+  docker-compose -f docker-compose-prod.yml down
+}
+
+# run all tests
+all() {
+  docker-compose up -d --build
+  docker-compose exec supplyit-users python manage.py test
+  inspect $? supplyit-users
+  docker-compose exec supplyit-users flake8 project
+  inspect $? supplyit-users-lint
+  docker-compose exec client npm run coverage
+  inspect $? client
+  docker-compose down
+  e2e
+}
+
+# run appropriate tests
+if [[ "${type}" == "server" ]]; then
+  echo "\n"
+  echo "Running server-side tests!\n"
+  server
+elif [[ "${type}" == "client" ]]; then
+  echo "\n"
+  echo "Running client-side tests!\n"
+  client
+elif [[ "${type}" == "e2e" ]]; then
+  echo "\n"
+  echo "Running e2e tests!\n"
+  e2e
+else
+  echo "\n"
+  echo "Running all tests!\n"
+  all
+fi
 
 # return proper code
 if [ -n "${fails}" ]; then
+  echo "\n"
   echo "Tests failed: ${fails}"
   exit 1
 else
+  echo "\n"
   echo "Tests passed!"
   exit 0
 fi
