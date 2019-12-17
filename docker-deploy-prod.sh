c262638adbb48319e65888f0cd3f52ada220deb4
argipap <argipapaefstathiou@gmail.com>
2019-12-17 23:38:14 +0200

    update new task definitions automatically for production

--- docker-deploy-prod.sh
+++ docker-deploy-prod.sh
@@ -24,28 +24,46 @@ then
       fi
     }
 
+    update_service() {
+      if [[ $(aws ecs update-service --cluster $cluster --service $service --task-definition $revision | $JQ '.service.taskDefinition') != $revision ]]; then
+        echo "Error updating service: ${service}."
+        return 1
+      fi
+    }
+
     deploy_cluster() {
 
+      cluster="supplyit-production-cluster"
+
       # users
+      service="supplyit-users-prod-service"
       template="ecs_users_prod_taskdefinition.json"
       task_template=$(cat "ecs/$template")
       task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $AWS_RDS_URI $PRODUCTION_SECRET_KEY)
       echo "$task_def"
       register_definition
+      # update users service with the new task definitions
+      update_service
 
       # client
+      service="supplyit-client-prod-service"
       template="ecs_client_prod_taskdefinition.json"
       task_template=$(cat "ecs/$template")
       task_def=$(printf "$task_template" $AWS_ACCOUNT_ID)
       echo "$task_def"
       register_definition
+      # update client service with the new task definitions
+      update_service
 
       # swagger
+      service="supplyit-swagger-prod-service"
       template="ecs_swagger_prod_taskdefinition.json"
       task_template=$(cat "ecs/$template")
       task_def=$(printf "$task_template" $AWS_ACCOUNT_ID)
       echo "$task_def"
       register_definition
+      # update users swagger with the new task definitions
+      update_service
 
     }
 
