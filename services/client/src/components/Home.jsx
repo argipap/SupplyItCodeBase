0d2416003f6f97a8ef1a3b5253c98b4a9f6911fb
jasoncg <Jason@grayhrb.home>
2020-01-13 18:14:24 +0100

    react-bootstrap-integration-landingHome

--- services/client/src/components/Home.jsx
+++ services/client/src/components/Home.jsx
@@ -1,30 +1,36 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import './mystyle.css';
 
 class Home extends Component {
-    constructor(props) {
-        super(props);
-    };
+	constructor(props) {
+		super(props);
+	}
 
-    render() {
-        return (
-            <div className="get-started">
-                <div className="container has-text-left">
-                    <div className="columns">
-                        <div className="column is-half">
-                            <h1 className="title is-3">ALL YOUR SUPPLIERS<br/>IN ONE PLACE</h1>
-                            <hr/>
-                            <br/>
-                            <p>Supplyit is a one-stop-shop for all your wholesale foodservice suppliers. Instant sign
-                                up, one order, one payment, zero headaches.</p>
-                            <br/>
-                            <Link to="/getStarted" className="button is-danger">GET STARTED</Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    };
+	render() {
+		return (
+			<Container className="intro">
+				<Row className="justify-content-md-center align-items-center">
+					<Col md={6}>
+						<h1>Supply IT - Όλοι οι αγαπημένοι προμηθευτές σας!</h1>
+						<hr />
+						<p>
+							Supplyit is a one-stop-shop for all your wholesale foodservice suppliers. Instant sign up,
+							one order, one payment, zero headaches.
+						</p>
+
+						<Link to="/getStarted">
+							<Button variant="info">GET STARTED</Button>
+						</Link>
+					</Col>
+                    <Col md={4}>
+                        <img src=""/>
+                    </Col>
+				</Row>
+			</Container>
+		);
+	}
 }
 
 export default Home;
--- services/client/src/components/Logout.jsx
+++ services/client/src/components/Logout.jsx
@@ -1,17 +1,45 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 
-class Logout extends Component {
-    componentDidMount() {
-        this.props.logoutUser();
-    };
-    render() {
-        return (
-            <div>
-                <p>You are now logged out. Click <Link to="/login">here</Link> to log back in.</p>
-            </div>
-        )
-    };
+function Logout(props) {
+	useEffect(() => {
+		props.logoutUser();
+	});
+
+	const [ redirect, setRedirect ] = useState(false);
+
+	useEffect(() => {
+		let timer1 = setTimeout(() => setRedirect(true), 1000);
+		return () => {
+			clearTimeout(timer1)
+		}
+	}, []);
+	return ( 
+		<div>
+			<p>
+				You are now logged out. Click <Link to="/login">here</Link> to log back in.
+			</p>
+			<Redirect to="/" />
+		</div>
+	);
 }
 
 export default Logout;
+
+// --Class version--
+// class Logout extends Component {
+// 	componentDidMount() {
+// 		this.props.logoutUser();
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div>
+// 				<p>
+// 					You are now logged out. Click <Link to="/login">here</Link> to log back in.
+// 				</p>
+// 				<Redirect to="/" />
+// 			</div>
+// 		);
+// 	}
+// }
