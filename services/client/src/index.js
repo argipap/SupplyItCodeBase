f5d176dd6883573c9b92c5aa35acca1e9a729805
jasoncg <Jason@grayhrb.home>
2020-03-17 00:37:23 +0100

    microfixes on nav, footer, toast, supplier page

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ScrollToTop from './components/microcomps/ScrollToTop';
 
 
 import App from "./App";
@@ -10,6 +11,7 @@ import App from "./App";
 ReactDOM.render((
     // new
     <Router>
+        <ScrollToTop />
         <App/>
     </Router>
 ), document.getElementById('root'));
